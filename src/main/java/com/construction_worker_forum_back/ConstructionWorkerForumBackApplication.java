d15be5ec6dcb862baa574b13d2700c095d69f668
bsyso <bsyso@softserveinc.com>
2022-10-13 18:58:12 +0200

    Initial commit

---
+++ src/main/java/com/construction_worker_forum_back/ConstructionWorkerForumBackApplication.java
@@ -0,0 +1,13 @@
+package com.construction_worker_forum_back;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConstructionWorkerForumBackApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ConstructionWorkerForumBackApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/construction_worker_forum_back/ConstructionWorkerForumBackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.construction_worker_forum_back;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConstructionWorkerForumBackApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
