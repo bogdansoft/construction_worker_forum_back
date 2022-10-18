3807d101bfe6fec1c61edeedacbbf2b427568bb9
Jakub Rogowski <jakerogowski33gmail.com>
2022-10-18 12:24:07 +0200

    dummy test

--- src/test/java/com/construction_worker_forum_back/ConstructionWorkerForumBackApplicationTests.java
+++ src/test/java/com/construction_worker_forum_back/ConstructionWorkerForumBackApplicationTests.java
@@ -1,5 +1,6 @@
 package com.construction_worker_forum_back;
 
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
@@ -8,6 +9,12 @@ class ConstructionWorkerForumBackApplicationTests {
 
     @Test
     void contextLoads() {
+    }
+
+    @Test
+    void dummyTest() {
+        int actual = 1;
+        Assertions.assertEquals(1, actual);
     }
 
 }
