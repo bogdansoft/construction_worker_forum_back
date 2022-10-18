7b2e618ccec53a6cbfe46806bb89f0682f12f81e
Jakub Rogowski <jakerogowski33gmail.com>
2022-10-18 17:23:30 +0200

    test of tests

--- src/test/java/com/construction_worker_forum_back/ConstructionWorkerForumBackApplicationTests.java
+++ src/test/java/com/construction_worker_forum_back/ConstructionWorkerForumBackApplicationTests.java
@@ -1,10 +1,14 @@
 package com.construction_worker_forum_back;
 
 import com.construction_worker_forum_back.model.entity.User;
+import com.construction_worker_forum_back.repository.UserRepository;
 import com.construction_worker_forum_back.service.UserService;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 @SpringBootTest
 class ConstructionWorkerForumBackApplicationTests {
@@ -12,20 +16,30 @@ class ConstructionWorkerForumBackApplicationTests {
     @Autowired
     UserService userService;
 
+    @Autowired
+    UserRepository userRepository;
+
     @Test
     void contextLoads() {
+    }
+
+    @BeforeEach
+    void setUp() {
+        userRepository.deleteAll();
     }
 
     @Test
     void saveUser() {
         User user = User.builder()
-                .username("user1234")
+                .username("jake")
                 .password("secret")
-                .email("user@example.com")
+                .email("jake@example.com")
                 .firstName("John")
                 .lastName("Doe")
                 .build();
 
         userService.register(user);
+
+        assertTrue(userRepository.existsByUsernameIgnoreCase(user.getUsername()));
     }
 }
