beb16b1344acc7942300c3609c762804bcdcf65f
Jakub Rogowski <jakerogowski33gmail.com>
2022-10-28 18:15:47 +0200

    ChatRoomRepository added.

---
+++ src/main/java/com/construction_worker_forum_back/repository/ChatRoomRepository.java
@@ -0,0 +1,10 @@
+package com.construction_worker_forum_back.repository;
+
+import com.construction_worker_forum_back.model.chat.ChatRoom;
+import org.springframework.data.mongodb.repository.MongoRepository;
+
+import java.util.Optional;
+
+public interface ChatRoomRepository extends MongoRepository<ChatRoom, String> {
+    Optional<ChatRoom> findBySenderIdAndRecipientId(String senderId, String recipientId);
+}
