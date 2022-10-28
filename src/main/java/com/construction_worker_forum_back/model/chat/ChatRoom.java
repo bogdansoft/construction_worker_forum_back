6ace65765903695364d44525f15dea53e8399504
Jakub Rogowski <jakerogowski33gmail.com>
2022-10-28 18:09:11 +0200

    Chatroom added.

--- src/main/java/com/construction_worker_forum_back/model/chat/ChatRoom.java
+++ src/main/java/com/construction_worker_forum_back/model/chat/ChatRoom.java
@@ -7,22 +7,15 @@ import lombok.NoArgsConstructor;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 
-import java.util.Date;
-
 @Data
 @Builder
 @Document
 @NoArgsConstructor
 @AllArgsConstructor
-public class ChatMessage {
+public class ChatRoom {
     @Id
     private String id;
     private String chatId;
     private String senderId;
     private String recipientId;
-    private String senderName;
-    private String recipientName;
-    private String content;
-    private Date timestamp;
-    private MessageStatus status;
 }
