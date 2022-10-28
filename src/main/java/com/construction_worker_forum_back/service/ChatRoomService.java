56116397d826e8d35a92ebb9d63701c6d5a53307
Jakub Rogowski <jakerogowski33gmail.com>
2022-10-28 18:26:31 +0200

    ChatRoomService added.

---
+++ src/main/java/com/construction_worker_forum_back/service/ChatRoomService.java
@@ -0,0 +1,42 @@
+package com.construction_worker_forum_back.service;
+
+import com.construction_worker_forum_back.model.chat.ChatRoom;
+import com.construction_worker_forum_back.repository.ChatRoomRepository;
+import lombok.AllArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+
+@Service
+@AllArgsConstructor
+public class ChatRoomService {
+
+    private final ChatRoomRepository chatRoomRepository;
+
+    public Optional<String> getChatId(String senderId, String recipientId, boolean createIfNotExists) {
+
+        return chatRoomRepository.findBySenderIdAndRecipientId(senderId, recipientId)
+                .map(ChatRoom::getChatId)
+                .or(() -> {
+                    if (!createIfNotExists) return Optional.empty();
+
+                    var chatId = String.format("%s_%s", senderId, recipientId);
+
+                    ChatRoom senderRecipient = ChatRoom.builder()
+                            .chatId(chatId)
+                            .senderId(senderId)
+                            .recipientId(recipientId)
+                            .build();
+
+                    ChatRoom recipientSender = ChatRoom.builder()
+                            .chatId(chatId)
+                            .senderId(recipientId)
+                            .recipientId(senderId)
+                            .build();
+
+                    chatRoomRepository.save(senderRecipient);
+                    chatRoomRepository.save(recipientSender);
+                    return Optional.of(chatId);
+                });
+    }
+}
