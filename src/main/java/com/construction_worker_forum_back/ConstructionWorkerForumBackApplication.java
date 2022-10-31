88f2fef330c311872500672ef9fb55db2a5870c9
Jakub Rogowski <jakerogowski33gmail.com>
2022-10-31 12:18:26 +0100

    manual testing in main app

--- src/main/java/com/construction_worker_forum_back/ConstructionWorkerForumBackApplication.java
+++ src/main/java/com/construction_worker_forum_back/ConstructionWorkerForumBackApplication.java
@@ -8,6 +8,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.context.annotation.Bean;
 
+import java.util.Optional;
+
 @SpringBootApplication
 @EntityScan(basePackages = {"com.construction_worker_forum_back.model.entity"})
 public class ConstructionWorkerForumBackApplication {
@@ -35,8 +37,15 @@ public class ConstructionWorkerForumBackApplication {
                     .recipientId(senderId)
                     .build();
 
-            chatRoomRepository.save(senderRecipient);
-            chatRoomRepository.save(recipientSender);
+            Optional<ChatRoom> fromDb1 = chatRoomRepository.findBySenderIdAndRecipientId(senderId, recipientId);
+            Optional<ChatRoom> fromDb2 = chatRoomRepository.findBySenderIdAndRecipientId(recipientId, senderId);
+            fromDb1.ifPresentOrElse(System.out::println,
+                    () -> System.out.println("Could not find")
+            );
+
+            fromDb2.ifPresentOrElse(System.out::println,
+                    () -> System.out.println("Could not find")
+            );
         };
     }
 }
