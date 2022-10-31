0bd653f933a49f7bb962e632c8f3d866d5bd0cde
Jakub Rogowski <jakerogowski33gmail.com>
2022-10-31 13:38:55 +0100

    Main app cleanup

--- src/main/java/com/construction_worker_forum_back/ConstructionWorkerForumBackApplication.java
+++ src/main/java/com/construction_worker_forum_back/ConstructionWorkerForumBackApplication.java
@@ -1,14 +1,8 @@
 package com.construction_worker_forum_back;
 
-import com.construction_worker_forum_back.model.chat.ChatRoom;
-import com.construction_worker_forum_back.repository.ChatRoomRepository;
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
-import org.springframework.context.annotation.Bean;
-
-import java.util.Optional;
 
 @SpringBootApplication
 @EntityScan(basePackages = {"com.construction_worker_forum_back.model.entity"})
@@ -16,36 +10,5 @@ public class ConstructionWorkerForumBackApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(ConstructionWorkerForumBackApplication.class, args);
-    }
-
-    @Bean
-    CommandLineRunner runner(ChatRoomRepository chatRoomRepository) {
-        return args -> {
-            String senderId = "senderId";
-            String recipientId = "recipientId";
-            String chatId = String.format("%s_%s", senderId, recipientId);
-
-            ChatRoom senderRecipient = ChatRoom.builder()
-                    .chatId(chatId)
-                    .senderId(senderId)
-                    .recipientId(recipientId)
-                    .build();
-
-            ChatRoom recipientSender = ChatRoom.builder()
-                    .chatId(chatId)
-                    .senderId(recipientId)
-                    .recipientId(senderId)
-                    .build();
-
-            Optional<ChatRoom> fromDb1 = chatRoomRepository.findBySenderIdAndRecipientId(senderId, recipientId);
-            Optional<ChatRoom> fromDb2 = chatRoomRepository.findBySenderIdAndRecipientId(recipientId, senderId);
-            fromDb1.ifPresentOrElse(System.out::println,
-                    () -> System.out.println("Could not find")
-            );
-
-            fromDb2.ifPresentOrElse(System.out::println,
-                    () -> System.out.println("Could not find")
-            );
-        };
     }
 }
