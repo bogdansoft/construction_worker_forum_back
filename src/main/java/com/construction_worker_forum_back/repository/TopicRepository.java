0ea55abd7e05db4075cc985d4934d0b32fda2d7d
Robert-Michalski <rmicha@softserveinc.com>
2022-11-03 14:24:59 +0100

    Add controller to search topics

--- src/main/java/com/construction_worker_forum_back/repository/TopicRepository.java
+++ src/main/java/com/construction_worker_forum_back/repository/TopicRepository.java
@@ -3,6 +3,7 @@ package com.construction_worker_forum_back.repository;
 import com.construction_worker_forum_back.model.entity.Topic;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
 import java.util.Optional;
 
 public interface TopicRepository extends JpaRepository<Topic, Long> {
@@ -10,4 +11,10 @@ public interface TopicRepository extends JpaRepository<Topic, Long> {
     int deleteTopicById(Long id);
 
     Optional<Topic> findTopicByName(String name);
+
+    List<Topic> findByNameContainsIgnoreCase(String name);
+
+
+
+
 }
