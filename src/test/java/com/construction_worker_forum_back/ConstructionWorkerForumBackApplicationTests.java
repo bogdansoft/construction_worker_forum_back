a0e51f7f9ce364da548d3663a0f6d9ef24ed89d3
Jakub Rogowski <jakerogowski33gmail.com>
2022-11-04 14:41:11 +0100

    refactor

--- src/test/java/com/construction_worker_forum_back/ConstructionWorkerForumBackApplicationTests.java
+++ src/test/java/com/construction_worker_forum_back/ConstructionWorkerForumBackApplicationTests.java
@@ -2,10 +2,8 @@ package com.construction_worker_forum_back;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.TestPropertySource;
 
 @SpringBootTest
-@TestPropertySource("classpath:application.properties")
 class ConstructionWorkerForumBackApplicationTests {
 
     @Test
