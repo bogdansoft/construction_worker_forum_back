9fca5766c8f56ea6386f1bd9f7dc5960ce068161
bsyso <bsyso@softserveinc.com>
2022-11-04 09:25:33 +0100

    added tests bug fix

--- src/test/java/com/construction_worker_forum_back/integration/RemoveService.java
+++ src/test/java/com/construction_worker_forum_back/integration/RemoveService.java
@@ -2,6 +2,7 @@ package com.construction_worker_forum_back.integration;
 
 import com.construction_worker_forum_back.repository.CommentRepository;
 import com.construction_worker_forum_back.repository.PostRepository;
+import com.construction_worker_forum_back.repository.TopicRepository;
 import com.construction_worker_forum_back.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
@@ -14,9 +15,11 @@ public class RemoveService {
     private final PostRepository postRepository;
     private final UserRepository userRepository;
     private final CommentRepository commentRepository;
+    private final TopicRepository topicRepository;
 
     @Transactional
     public void removeAll() {
+        topicRepository.deleteAll();
         postRepository.deleteAll();
         userRepository.deleteAll();
         commentRepository.deleteAll();
