3a33a788a6a8c42a2ff9a8ad913c8dc8896e6dee
bsyso <bsyso@softserveinc.com>
2022-11-04 14:23:13 +0100

    added environments variables

--- src/test/java/com/construction_worker_forum_back/ConstructionWorkerForumBackApplicationTests.java
+++ src/test/java/com/construction_worker_forum_back/ConstructionWorkerForumBackApplicationTests.java
@@ -2,8 +2,10 @@ package com.construction_worker_forum_back;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.TestPropertySource;
 
 @SpringBootTest
+@TestPropertySource("classpath:application.properties")
 class ConstructionWorkerForumBackApplicationTests {
 
     @Test
