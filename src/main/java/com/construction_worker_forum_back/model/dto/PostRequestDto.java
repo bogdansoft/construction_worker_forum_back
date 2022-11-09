714ba0165333eb2eb63834003b490e0c6929aa01
Robert-Michalski <rmicha@softserveinc.com>
2022-11-09 12:53:34 +0100

    add tests for post controller

--- src/main/java/com/construction_worker_forum_back/model/dto/PostRequestDto.java
+++ src/main/java/com/construction_worker_forum_back/model/dto/PostRequestDto.java
@@ -1,6 +1,7 @@
 package com.construction_worker_forum_back.model.dto;
 
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
@@ -10,6 +11,7 @@ import javax.validation.constraints.Size;
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
+@Builder
 public class PostRequestDto {
 
     @NotNull
