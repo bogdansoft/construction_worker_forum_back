7bbcaf05d9677e172ab53eec6a0e832dcd71054d
Dawid Kozłowski <dkozlow@softserveinc.com>
2022-11-09 11:13:06 +0100

    small improvments

---
+++ src/test/java/com/construction_worker_forum_back/service/CommentServiceTest.java
@@ -0,0 +1,10 @@
+package com.construction_worker_forum_back.service;
+
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.test.context.ActiveProfiles;
+
+@ActiveProfiles("dev")
+@ExtendWith(MockitoExtension.class)
+public class CommentServiceTest {
+}
