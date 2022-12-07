0cf0df6991a9dd0bd07c67a201821910dbb2a8db
Dawid Kozłowski <dkozlow@softserveinc.com>
2022-12-07 10:35:03 +0100

    resolving conflicts

--- src/main/java/com/construction_worker_forum_back/model/dto/UserDto.java
+++ src/main/java/com/construction_worker_forum_back/model/dto/UserDto.java
@@ -5,8 +5,10 @@ import com.construction_worker_forum_back.model.dto.simple.PostSimpleDto;
 import com.construction_worker_forum_back.model.security.AccountStatus;
 import com.construction_worker_forum_back.model.security.Role;
 import lombok.*;
+
 import org.springframework.cache.annotation.Cacheable;
-import java.io.*;
+import java.io.Serial;
+import java.io.Serializable;
 import java.util.Date;
 import java.util.List;
 
@@ -18,7 +20,6 @@ import java.util.List;
 @NoArgsConstructor
 @Cacheable
 public class UserDto implements Serializable {
-
     @Serial
     private static final long serialVersionUID = -6470090944414208496L;
     private Long id;
@@ -28,7 +29,6 @@ public class UserDto implements Serializable {
     private String lastName;
     private String bio;
     private String avatar;
-    private byte[] avatarBytes;
     private Date createdAt;
     private Date updatedAt;
     private AccountStatus accountStatus;
